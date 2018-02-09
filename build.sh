52067b0f05130815c0b24849b7a823935cee8be3
Jim Robinson <jscrobinson@gmail.com>
2018-02-09 16:31:28 +0000

    Improve build file

--- build.sh
+++ build.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 
 VERSION=${1:-"dev"}
-
-echo "Building croudtech/drone-docker:$VERSION docker image"
+FLAVOURS=${2:-"docker,ecr"}
 
 docker run --rm -v "$PWD":/go/src/github.com/CroudSupport/drone-docker -w /go/src/github.com/CroudSupport/drone-docker golang:1.8 ./.drone.sh
-docker build . --file ./docker/Dockerfile -t croudtech/drone-docker:$VERSION
+
+for FLAVOUR in $(echo $FLAVOURS | sed "s/,/ /g")
+do
+    if [ $FLAVOUR = "docker" ] ; then
+    DOCKERFILE_PATH="./docker/Dockerfile"
+    else
+        DOCKERFILE_PATH="./docker/$FLAVOUR/Dockerfile"
+    fi
+
+    if [ -f $DOCKERFILE_PATH ]; then
+        IMAGE="croudtech/drone-docker:$FLAVOUR-$VERSION"
+        echo "Building $IMAGE docker image"
+        docker build . --file $DOCKERFILE_PATH -t $IMAGE
+    fi
+
+done
+
